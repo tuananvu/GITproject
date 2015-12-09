e6ab6c68a6ee2171b2d2c5c428ebdd6764855fa5
An Nguyen <hongantcnguyen@yahoo.com>
2015-12-09 11:23:55 -0500

    add time-aware feature (TimeSlider) to initMap.js and webmap4.html

--- WebContent/arcgis/js/my/initMap.js
+++ WebContent/arcgis/js/my/initMap.js
@@ -2,19 +2,25 @@ define([
         "esri/map",
         "esri/layers/ArcGISDynamicMapServiceLayer", 
         "esri/layers/FeatureLayer",
-        "esri/InfoTemplate"
+        "esri/InfoTemplate",
+        "esri/TimeExtent",
+        "esri/dijit/TimeSlider",
+        "dojo/dom"
 ], function(
 		Map,
 		ArcGISDynamicMapServiceLayer, 
 		FeatureLayer,
-		InfoTemplate){
+		InfoTemplate,
+		TimeExtent,
+		TimeSlider,
+		dom){
 	var vMap;
 	
 	return {
 		init:function(){
 			vMap = new Map("mapDiv", {
 				basemap: "topo",
-				center: [-46,37]
+				center: [-46,37],				
 				//infoWindow: popup			
 			});	
 			var _oilAndGasInfoTemplate = new InfoTemplate();
@@ -60,6 +66,29 @@ define([
 			obs_situ_geolink_aq_layer.hide();				
 			obs_situ_geolink_radiosonde_layer.hide();
 			oilAndGasLayer.hide();
+			
+			var timeSlider = new TimeSlider({
+	            style: "width: 100%;"
+	          }, dom.byId("timeSliderDiv"));
+			vMap.setTimeSlider(timeSlider);
+			 var timeExtent = new TimeExtent();
+			currentDate = new Date();
+	          timeExtent.startTime = new Date(currentDate.getTime() - (4*60*60*1000)); //4 hour before present
+	          timeExtent.endTime = currentDate;
+	          timeSlider.setThumbCount(1);
+	          timeSlider.createTimeStopsByCount(timeExtent,5);
+	          //timeSlider.createTimeStopsByTimeInterval(timeExtent, 2, "esriTimeUnitsYears");
+	          //timeSlider.setThumbIndexes([0,1]);
+	          //timeSlider.setThumbMovingRate(2000);
+	          timeSlider.singleThumbAsTimeInstant(true);
+	          //timeSlider.setTickCount(4);
+	         
+	          timeSlider.startup();
+	          timeSlider.on("time-extent-change", function(evt) {
+	              //var startValString = evt.startTime.getUTCFullYear();
+	              //var endValString = evt.endTime.getUTCFullYear();
+	              //dom.byId("daterange").innerHTML = "<i>" + startValString + " and " + endValString  + "<\/i>";
+	            });
 		},
 		getMap:function(){
 			return vMap;
